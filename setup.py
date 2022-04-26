7b045674e557139570ecbb95b10e8ab151a1619f
Han Qiao <qiao.han@aicadium.ai>
2022-04-26 16:13:10 +0800

    Bump release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="streamlit_supabase_auth",
-    version="1.0.1",
+    version="1.0.2",
     author="Han Qiao",
     author_email="sweatybridge@gmail.com",
     description="JWT authentication with Supabase",
